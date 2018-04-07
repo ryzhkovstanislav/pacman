6a774747114a4dfff3fce3310cc4755d390f715b
Stanislav Ryzhkov <stanislav.rv@gmail.com>
2018-04-07 16:33:58 +0600

    Андроид-игра "Пакман"

---
+++ AndroidPacman/core/src/com/pacman/game/MyGdxGame.java
@@ -0,0 +1,34 @@
+package com.pacman.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class MyGdxGame extends Game {
+    SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        ScreenManager.getInstance().init(this, batch);
+        ScreenManager.getInstance().changeScreen(ScreenManager.ScreenType.MENU);
+    }
+
+    @Override
+    public void render() {
+        float dt = Gdx.graphics.getDeltaTime();
+        getScreen().render(dt);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
